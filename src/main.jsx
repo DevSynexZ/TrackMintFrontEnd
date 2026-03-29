6897bf8a9f3f2abd8900c8da4e3ba7d05bd690e9
unknown <synexshahriar43@gmail.com>
2026-03-29 23:25:21 +0600

    Style Changed folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { PiggyProvider } from "./context/PiggyContext";
+import "../styles/style.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
